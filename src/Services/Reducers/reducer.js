46feceb4d61379012ebd9beced63e1ca8866176e
anjali-2911 <96237256+anjali-2911@users.noreply.github.com>
2024-08-28 23:31:06 +0530

    testing item

--- src/Services/Reducers/reducer.js
+++ src/Services/Reducers/reducer.js
@@ -15,6 +15,7 @@ export default function cardItems(state = [], action) {
                 console.warn("reducer remove",action)
                 state.pop();
                return [
+                
                    ...state,
                ]
         default:
