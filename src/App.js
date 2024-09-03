66e777df6a06fd0c57fb0b3fc485ad3aeb960ac3
anjali-2911 <96237256+anjali-2911@users.noreply.github.com>
2024-09-03 13:30:04 +0530

    profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HomeContainer from './Containers/HomeContainer'
 import HeaderComponents from './Containers/HeaderComponent'
+import Profile from './Profile/Profile';
+Profile
 function App() {
   return (
     <div className="App">
       <HeaderComponents />
      
       <HomeContainer />
+      <Profile/>
     </div>
   );
 }
